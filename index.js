1653defbe110b14d4eba8fd229e6255a509b7ff1
nrenner <nrenner0211@gmail.com>
2022-07-05 21:42:53 -0400

    index functional

--- index.js
+++ index.js
@@ -1,9 +1,73 @@
 // packages needed
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/Employee');
+const Employee = require('./lib/Employee.js');
 
 // welcome
 console.log("Welcome to fictional-happiness Corporation's employee tracker! It will prompt you for information on employees, classify them by role, and generate a webpage with easy-to-access employee cards.");
 
-new Employee().init();
+// array of questions
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Please provide employee's full name. (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a name!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Please provide employee's ID/Badge number.",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log('Please enter an ID number!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please provide a valid email address to reach the employee. (Required)",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter an email address!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: "What is this employee's role?",
+        choices: ['Engineer', 'Intern', 'Manager'],
+        validate: roleInput => {
+            if (roleInput == 'Engineer') {
+                this.Employee.push(new Engineer('github', 'role'));
+            }
+        }
+    }
+];
+
+init = function() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput);
+    })
+};
+
+init();
+
+
+
